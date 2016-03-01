d30791ac3c079d50d369fd8fa8d8b6ae3d972614
gabiiz <gabrielascardine@gmail.com>
2016-03-01 01:44:54 -0500

    Importing (via npm) bootstrap v4 instead of v3, but keeping main.css

--- app.js
+++ app.js
@@ -110,6 +110,15 @@ app.locals = {
 }
 
 /**
+ * Add routes to js and css folders
+ */
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/js', express.static(__dirname + '/public/js')); // redirect public JS
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+app.use('/css', express.static(__dirname + '/public/css')); // redirect public CSS
+
+/**
  * Primary app routes.
  */
 app.get('/', homeController.index);
